84569bcf3a25c36cd36796185f2bdeefc4f1898f
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-12-03 17:24:47 -0800

    Adds shell string parser

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,12 @@ extern "C" {
 pub fn greet() {
     alert("Hello, web-term!");
 }
+
+#[wasm_bindgen]
+pub fn split_term_text(text: &str) -> Result<Vec<String>, JsValue> {
+    match shellwords::split(text) {
+        Ok(v) => Ok(v),
+        Err(e) => Err(JsValue::from_str(&format!("{}", e))),
+    }
+}
+
